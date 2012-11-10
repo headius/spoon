9c2bfb3583a7847017fec4344bf14fcf016b55a0
Daniel Cavanagh <danielcavanagh85@gmail.com>
2012-11-10 20:41:43 +1000

    make the windows code work properly after the merge, and also get the proper errno instead of the fake one

--- lib/spoon/windows.rb
+++ lib/spoon/windows.rb
@@ -3,10 +3,14 @@
 require 'ffi'
 
 module Spoon
+  extend FFI::Library
+  
   P_NOWAIT = 1
   
+  ffi_lib 'c'
   attach_function :_spawnve, [:int, :string, :pointer, :pointer], :int
   attach_function :_spawnvpe, [:int, :string, :pointer, :pointer], :int
+  attach_function :_get_errno, [:pointer], :errno_t
   
   ffi_lib 'kernel32'
   ffi_convention :stdcall
@@ -14,13 +18,13 @@ module Spoon
   
   def self.spawn(*args)
     handle = _spawnve(*_prepare_spawn_args(args))
-    raise SystemCallError.new(args[0], FFI.errno) if handle == -1
+    _raise_error(args[0]) if handle == -1
     _get_process_id(handle)
   end
   
   def self.spawnp(*args)
     handle = _spawnvpe(*_prepare_spawn_args(args))
-    raise SystemCallError.new(args[0], FFI.errno) if handle == -1
+    _raise_error(args[0]) if handle == -1
     _get_process_id(handle)
   end
   
@@ -36,5 +40,11 @@ module Spoon
     env_ary.put_array_of_pointer(0, env_ptrs)
     
     [P_NOWAIT, args[0], args_ary, env_ary]
+  end
+  
+  def self._raise_error(error)
+    errno = FFI::MemoryPointer.new(:int)
+    _get_errno(errno)
+    raise SystemCallError.new(error, errno.get_int)
   end
 end
--- spoon.gemspec
+++ spoon.gemspec
@@ -4,7 +4,7 @@ Gem::Specification.new do |s|
   s.authors = ["Charles Oliver Nutter"]
   s.date = "2012-04-20"
   s.description = s.summary = "Spoon is an FFI binding of the posix_spawn function (and Windows equivalent), providing fork+exec functionality in a single shot."
-  s.files = "lib/spoon.rb"
+  s.files = ["lib/spoon.rb", "lib/spoon/unix.rb", "lib/spoon/windows.rb"]
   s.require_paths = ["lib"]
   s.add_dependency('ffi') unless defined?(JRUBY_VERSION) # JRuby includes ffi
 end
